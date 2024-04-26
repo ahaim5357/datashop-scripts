744ac310b03f8ec8082def6fb927750112e73ffa
Aaron Haim <ahaim@ashwork.net>
2024-04-26 11:18:34 -0400

    chore(shell): Update QA URL info

--- shell/datashop-test.sh
+++ shell/datashop-test.sh
@@ -3,6 +3,8 @@
 DATASHOP_ACCESS_KEY_ID=""
 DATASHOP_SECRET_ACCESS_KEY=""
 
+# QA URL: https://pslc-qa.andrew.cmu.edu/datashop/
+
 # TODO: Check .env in project, Check ~/.datashop/credentials, Check $DATASHOP_ACCESS_KEY_ID; $DATASHOP_SECRET_ACCESS_KEY
 # Read in environment variables
 
@@ -15,7 +17,6 @@ contentMD5=""
 contentType=""
 timestamp=`TZ=GMT date "+%a, %d %b %Y %H:%M:%S %Z"`
 uripath="/datasets/1"
-# QA URL: https://pslc-qa.andrew.cmu.edu/
 # Base64#encodeBase64 when isChunked is true, adds carriage return (\r\n) after every 76 bytes
 # TODO: Remove jq requirement
 apisig=`printf "%s\n%s\n%s\n%s\n%s" "${method}" "${contentMD5}" "${contentType}" "${timestamp}" "${uripath}" | openssl dgst -binary -sha1 -hmac "${DATASHOP_SECRET_ACCESS_KEY}" | base64 | sed 's/.\{76\}/&\r/g'`
